cd7b72fd09d3372dd16a2f5c50cdf4e80411216a
nishthapant <48161605+nishthapant@users.noreply.github.com>
2021-09-08 12:47:22 -0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -3,7 +3,7 @@ import requests as rq
 response = rq.get("https://raw.githubusercontent.com/nishthapant/cmput404/master/lab1.py")
 
 # download the script from raw Github url and save it
-filePath = "dowloaded.py"
+filePath = "downloaded.py"
 f = open(filePath, "wb")
 f.write(response.content)
 f.close()

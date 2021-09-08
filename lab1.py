de75811d92f4e03833f7dd4b6e51f5cf8432b0bf
Nishtha <nishpant2015@gmail.com>
2021-09-08 12:36:17 -0600

    chore: script can download itself from github

--- lab1.py
+++ lab1.py
@@ -1,3 +1,15 @@
 import requests as rq
 
-rq.get("https://www.google.com")
+response = rq.get("https://raw.githubusercontent.com/nishthapant/cmput404/master/lab1.py")
+
+# download the script from raw Github url and save it
+filePath = "dowloaded.py"
+f = open(filePath, "wb")
+f.write(response.content)
+f.close()
+
+# print source code from downloaded file
+f = open(filePath, "r")
+print(f.read())
+f.close()
+
